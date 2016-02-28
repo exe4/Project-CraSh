ea92ebb1e4b1add8a76dae517af63202836087b9
Pranoy Dutta <Arindum@2014_PRANOY.home>
2016-02-28 13:29:56 -0600

    fullScreen available...

--- src/net/exe/game/Main.java
+++ src/net/exe/game/Main.java
@@ -1,10 +1,14 @@
 package net.exe.game;
 
+import net.exe.window.GameWindow;
+
 public class Main {
 	
 	
 	public static void main(String args[]){
-		 
+		 GameWindow frame = new GameWindow("Project CraSH", 1280, 720);
+		 frame.setFullScreen(1);
+		 frame.setVisible(true);
 	}
 	
 	
--- src/net/exe/window/GameWindow.java
+++ src/net/exe/window/GameWindow.java
@@ -1,5 +1,8 @@
 package net.exe.window;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import javax.swing.JFrame;
 
 public class GameWindow extends JFrame{
@@ -11,13 +14,49 @@ public class GameWindow extends JFrame{
 	 * 
 	 * This JFrame Class is fully set up for the game
 	 */
+	
+	boolean fullScreenEnabled = false;
+	int fullScreeMode = 0;
+	
+	GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
+	
 	private static final long serialVersionUID = 1L;
 
 	public GameWindow(String title, int width, int height) {
 		super(title);
 		setSize(width, height);
+		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
+		setResizable(false);
+	}
+	
+	private void setfullScreen(){
+		switch(fullScreeMode){
+		case 0:
+			System.out.println("No FullScreen...");
+			setUndecorated(false);
+			break;
+		case 1:
+			setUndecorated(true);
+			setExtendedState(JFrame.MAXIMIZED_BOTH);
+			break;
+		case 2:
+			setUndecorated(true);
+			device.setFullScreenWindow(this);
+			break;
+		}
+	}
+	
+	public void setFullScreen(int fullScreenNewMode){
+		fullScreenEnabled = true;
+		if(fullScreeMode <= 2){
+			this.fullScreeMode = fullScreenNewMode;
+			setfullScreen();
+		}else{
+			System.err.println("Error -> " + fullScreeMode + " is not supported... \nError Code 93");
+		}
+		
+		
 	}
 
 }
