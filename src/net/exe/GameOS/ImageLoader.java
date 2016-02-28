73a6a0f1b7cd21f26c2b6c73e3c36b2794ceafec
Pranoy Dutta <Arindum@2014_PRANOY.home>
2016-02-28 16:04:02 -0600

    Added ImageLoader Class

---
+++ src/net/exe/GameOS/ImageLoader.java
@@ -0,0 +1,26 @@
+package net.exe.GameOS;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+public class ImageLoader {
+
+	public static BufferedImage loadImage(Class<?> classFile, String Path) {
+		
+		URL url = classFile.getResource(Path);
+		BufferedImage img = null;
+		
+		try{
+			img = ImageIO.read(url);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		return img;
+		
+	}
+
+}
