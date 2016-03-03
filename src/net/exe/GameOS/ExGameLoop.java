803fa766ce0bba08c858fb9e611c8404cea6fb28
Pranoy Dutta <Arindum@2014_PRANOY.home>
2016-03-02 21:47:18 -0600

    Added more to Game loop

--- src/net/exe/GameOS/ExGameLoop.java
+++ src/net/exe/GameOS/ExGameLoop.java
@@ -1,6 +1,10 @@
 package net.exe.GameOS;
 
-public class ExGameLoop implements Runnable{
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class ExGameLoop extends JPanel implements Runnable{
 	
 	private Thread thread;
 	private boolean running;
@@ -8,14 +12,37 @@ public class ExGameLoop implements Runnable{
 	// frames per second, ticks per second, width, and height
 	private int fps, tps, width, height; 
 	
-	public static double currentFps = 60D;
+	public static double currentFps = 120D;
 	
-	public ExGameLoop() {
+	public ExGameLoop(int width, int height) {
+		this.width = width;
+		this.height = height;
 		
+		setPreferredSize( new Dimension(width, height) );
+		setFocusable(false);
+		requestFocus();
+	}
+	
+	@Override
+	public void addNotify() {
+		super.addNotify();
+		
+		if(thread == null ){
+			thread = new Thread(this);
+			thread.start();
+		}
 	}
 	
 	@Override
 	public void run(){
+		
+		/*init*/
+		long lastTime = System.nanoTime();
+		double nsecondsPerTick = 1000000000D;
+		int frames = 0;
+		int ticks = 0;
+		long lastTimer = System.currentTimeMillis();
+		
 		
 	}
 }
